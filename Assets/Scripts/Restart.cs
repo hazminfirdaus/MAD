9b7e30fbf9c7e82d611a8756a58c8d99346fc950
hazminfirdaus <cmhazminfirdaus@gmail.com>
2023-06-02 15:14:54 +0200

    comment on Restart.cs

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// function to restart the game
 public class Restart : MonoBehaviour
 {
     public void RestartGame()
